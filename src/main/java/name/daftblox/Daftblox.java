7d64f6301e1c32cda8b53e821726922efbd6ca8f
James Vo <48601146+Eagleverse@users.noreply.github.com>
2023-12-19 17:45:27 -0600

    first mod items

---
+++ src/main/java/name/daftblox/Daftblox.java
@@ -0,0 +1,34 @@
+package name.daftblox;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Daftblox implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	// an instance of our new item
+	// an instance of our new item
+	public static final fabric CUSTOM_ITEM =
+			Registry.register(Registries.ITEM, new Identifier("daftblox", "fabric"),
+		new fabric(new FabricItemSettings()));
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("daftblox");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
